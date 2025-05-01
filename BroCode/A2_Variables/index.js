688d7732a859842dfc853acdc7803f44a7269a54
keshav <k21keshav05@gmail.com>
2025-05-02 00:04:00 +0530

    Variables

--- BroCode/A2_Variables/index.js
+++ BroCode/A2_Variables/index.js
@@ -6,12 +6,17 @@ let age = 45;                   //Number
 let gpa = 6.9;                  //Float
 let online = true;              //Boolean
 
+console.log(`typeof name -> `+typeof name);
+console.log(`typeof age -> `+typeof age);
+console.log(`typeof gpa -> `+typeof gpa);
+console.log(`typeof online -> `+typeof online);
+
 
 document.getElementById("h1").textContent = `Hi ${name} !`
 document.getElementById("p1").textContent = `your name is:  ${name} !`
 document.getElementById("p2").textContent = `your age is:  ${age} !`
 document.getElementById("p3").textContent = `your gpa is:  ${gpa} !`
-document.write("use 'typeof' to find datatype of variable ex: typeof name -> String");
+
 
 if(online){
     document.getElementById("p4").textContent = `You are Online !`
