8b56740b821e435be0b4fc646bfb1340163cb182
keshav <k21keshav05@gmail.com>
2025-05-01 23:26:22 +0530

    FirstWebsite

---
+++ BroCode/A1_FirstWebsite/index.js
@@ -0,0 +1,6 @@
+console.log("hello this is console.log(\"\")");
+
+window.alert("Hello, this is a window.alert(\"\")");
+
+document.getElementById("myH1").textContent = "document.getElementById(\"myH1\").textContent(\"\")";
+document.getElementById("myPara").textContent = "document.getElementById(\"myPara\").textContent(\"\")";
