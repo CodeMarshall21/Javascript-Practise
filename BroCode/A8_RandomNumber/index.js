5b5ab6c5dd25576f00828251da249242763f8f9d
keshav <k21keshav05@gmail.com>
2025-05-02 22:31:50 +0530

    Random Number

--- BroCode/A8_RandomNumber/index.js
+++ BroCode/A8_RandomNumber/index.js
@@ -1,1 +1,26 @@
 // RANDOM NUMBER GENERATOR
+
+const label2 =  document.getElementById("label2");
+const label1 =  document.getElementById("label1");
+const label3 =  document.getElementById("label3");
+
+let random1;
+let random2;
+let random3;
+
+
+const MIN = 0;
+const MAX = 6;
+
+
+
+document.getElementById("roll").onclick = function(){
+    random1 = Math.floor(Math.random() * max) + min     //  <- Math.random() generated num btw 0 - 1
+    random2 = Math.floor(Math.random() * max) + min     //  <- Math.random() generated num btw 0 - 1
+    random3 = Math.floor(Math.random() * max) + min     //  <- Math.random() generated num btw 0 - 1
+
+    label1.textContent = random1;
+    label2.textContent = random2;
+    label3.textContent = random3;
+
+}
