85fda63d69abcd66a63798aba05b16ca3e24fd4d
keshav <k21keshav05@gmail.com>
2025-05-02 22:44:23 +0530

    Random Number Generator

--- BroCode/A8_RandomNumber/index.js
+++ BroCode/A8_RandomNumber/index.js
@@ -1,7 +1,7 @@
 // RANDOM NUMBER GENERATOR
 
-const label2 =  document.getElementById("label2");
 const label1 =  document.getElementById("label1");
+const label2 =  document.getElementById("label2");
 const label3 =  document.getElementById("label3");
 
 let random1;
@@ -9,18 +9,20 @@ let random2;
 let random3;
 
 
-const MIN = 0;
+const MIN = 1;
 const MAX = 6;
 
 
 
 document.getElementById("roll").onclick = function(){
-    random1 = Math.floor(Math.random() * max) + min     //  <- Math.random() generated num btw 0 - 1
-    random2 = Math.floor(Math.random() * max) + min     //  <- Math.random() generated num btw 0 - 1
-    random3 = Math.floor(Math.random() * max) + min     //  <- Math.random() generated num btw 0 - 1
+    random1 = Math.floor(Math.random() * MAX) + MIN;     //  <- Math.random() generated num btw 0 - 1
+    random2 = Math.floor(Math.random() * MAX) + MIN;     //  <- Math.random() generated num btw 0 - 1
+    random3 = Math.floor(Math.random() * MAX) + MIN;     //  <- Math.random() generated num btw 0 - 1
 
     label1.textContent = random1;
     label2.textContent = random2;
     label3.textContent = random3;
 
 }
+
+// label1.textContent = "hello";
