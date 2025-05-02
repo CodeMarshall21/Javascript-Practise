1de93b7aa42b4035de5d39b56c173a0794280fe4
keshav <k21keshav05@gmail.com>
2025-05-02 16:31:01 +0530

    Type Conversion

--- BroCode/A5_TypeConversion/index.js
+++ BroCode/A5_TypeConversion/index.js
@@ -9,24 +9,27 @@ let bool;
 
 document.getElementById("submit").onclick = function(){
     val = document.getElementById("inp").value;
-    document.getElementById("myh1").textContent = `Datatype: ${typeof val}`
+    document.getElementById("myh1").textContent = `Datatype: ${typeof val}`;
 }
 
 document.getElementById("number").onclick = function(){
     num = Number(val);
-    document.getElementById("myh1").textContent = `Datatype: ${typeof num}`
-    document.getElementById("para").textContent = `Datatype converted to: ${typeof num}; val = ${num}`
+    document.getElementById("myh1").textContent = `Datatype: ${typeof num}`;
+    window.alert(`Datatype converted to: ${typeof num};`);
+    document.getElementById("para").textContent = `val = ${num}`;
 }
 
 document.getElementById("string").onclick = function(){
     str = String(val);
-    document.getElementById("myh1").textContent = `Datatype: ${typeof str}`
-    document.getElementById("para").textContent = `Datatype converted to: ${typeof str}; val = ${str}`
+    document.getElementById("myh1").textContent = `Datatype: ${typeof str}`;
+    window.alert(`Datatype converted to: ${typeof str};`);
+    document.getElementById("para").textContent = `val = ${str}`;
 }
 
 document.getElementById("boolean").onclick = function(){
     bool = Boolean(val);
-    document.getElementById("myh1").textContent = `Datatype: ${typeof bool}`
-    document.getElementById("para").textContent = `Datatype converted to: ${typeof bool}; val = ${bool}`
+    document.getElementById("myh1").textContent = `Datatype: ${typeof bool}`;
+    window.alert(`Datatype converted to: ${typeof bool};`);
+    document.getElementById("para").textContent = `val = ${bool}`;
 }
 
