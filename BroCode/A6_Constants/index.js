b1b0299c4bee61383471ff08b50f1035b6922102
keshav <k21keshav05@gmail.com>
2025-05-02 16:51:58 +0530

    Constants

--- BroCode/A6_Constants/index.js
+++ BroCode/A6_Constants/index.js
@@ -1,1 +1,21 @@
 // const = a variable that can't be changed
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+
+/* 
+    
+    PI = 420.69;
+    Uncaught TypeError: Assignment to constant variable. 
+
+ */
+
+
+document.getElementById("calculate").onclick = function(){
+    radius = Number(document.getElementById("radius").value);
+    circumference = 2 * PI * radius;
+    console.log(circumference);
+    document.getElementById("myh3").textContent = `Circumference: ${circumference}`;
+}
