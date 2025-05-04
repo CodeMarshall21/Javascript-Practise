f94bd6b8f489786f3e06632f767761640d2182d4
keshav <k21keshav05@gmail.com>
2025-05-05 02:49:29 +0530

    Strict Equality

--- BroCode/A18_StrictEquality/index.js
+++ BroCode/A18_StrictEquality/index.js
@@ -4,11 +4,55 @@
 //  != inequality operator
 // !== strict inequality operator
 
-const PI = 3.14;
+const intPI = document.getElementById("integer");
+const strPI = document.getElementById("string");
 
-if(PI === "3.14"){
-    console.log("That is NOT Pi");
+const result = document.getElementById("result");
+
+const sInt = document.getElementById("strictlyint");
+const sStr = document.getElementById("strictlystring");
+
+const isInt = document.getElementById("isint");
+const isStr = document.getElementById("isstring");
+
+const submit = document.getElementById("Mysubmit");
+
+let PI;
+
+submit.onclick = function(){
+    if (intPI.checked){
+        PI = 3.14;
+        if (sInt.checked){
+            result.textContent = PI === 3.14;
+        }
+        else if (sStr.checked){
+            result.textContent = PI === "3.14";
+        }
+        else if (isInt.checked){
+            result.textContent = PI == 3.14;
+        }
+        else if (isStr.checked){
+            result.textContent = PI == "3.14";
+        }
+    }
+
+
+    else if (strPI){
+        PI = "3.14";
+        if (sInt.checked){
+            result.textContent = PI === 3.14;
+        }
+        else if (sStr.checked){
+            result.textContent = PI === "3.14";
+        }
+        else if (isInt.checked){
+            result.textContent = PI == 3.14;
+        }
+        else if (isStr.checked){
+            result.textContent = PI == "3.14";
+        }
+    }
+
 }
-else{
-    console.log("That is Pi");
-}
+
+
