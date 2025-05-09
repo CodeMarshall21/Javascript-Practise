1ba565771e63d8302712c9eda4ba25f7b4cc0e6a
keshav <k21keshav05@gmail.com>
2025-05-09 23:49:28 +0530

    Functions

---
+++ BroCode/A22_Functions/index.js
@@ -0,0 +1,17 @@
+// function = A section of reusable code.
+//                    Declare code once, use it whenever you want.
+//                    Call the function to execute that code.
+
+function isValid(email){
+    return email.contains("@") && email.comtains(".") ? true : false ;
+};
+
+document.getElementById("check").onclick = function(){
+    let email = document.getElementById("myEmail").value;
+    if (isValid(email)){
+        document.getElementById("result").textContent = "Your Email ID is verified !";
+    }
+    else{
+        document.getElementById("result").textContent = "Your Email ID is NOT verified !";
+    }
+}
