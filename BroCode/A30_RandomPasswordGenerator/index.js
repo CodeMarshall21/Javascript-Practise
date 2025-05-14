4bb5e7c8fed349c0dd78721b51e663a296f0ec67
keshav <k21keshav05@gmail.com>
2025-05-15 01:47:48 +0530

    Random Password Generator - JS Done

---
+++ BroCode/A30_RandomPasswordGenerator/index.js
@@ -0,0 +1,44 @@
+const passwordLen = document.getElementById("pwLen");
+const LowerCaseBtn = document.getElementById("IncLowerCase");
+const UpperCaseBtn = document.getElementById("IncUpperCase");
+const IncSymbolsBtn = document.getElementById("IncSymbols"); 
+const IncNumbersBtn = document.getElementById("IncNumbers");
+const passwordGenerateBtn = document.getElementById("pwGen");
+const passwordResult = document.getElementById("password");
+
+let generateArr = [];
+
+const passwordArray = 
+                    [
+                        "abcdefghijklmnopqrstuvwxyz",
+                        "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                        "0123456789",
+                        "!@#$%^&*()_+-=[]';,./{}|:\"<>?",
+                    ]
+
+
+passwordGenerateBtn.onclick = function generatePassword(){
+    if (LowerCaseBtn.checked){
+        generateArr.push(passwordArray[0]);
+    }
+    if (UpperCaseBtn.checked){
+        generateArr.push(passwordArray[1]);
+    }
+    if (IncSymbolsBtn.checked){
+        generateArr.push(passwordArray[3]);
+    }
+    if (IncNumbersBtn.checked){
+        generateArr.push(passwordArray[2]);
+    }
+
+    let password = ``;
+    let len = generateArr.length;
+
+    for (let i = 0; i <= passwordLen.value; i ++){
+        password += generateArr[i % len][Math.floor(Math.random() * (generateArr[i % len].length))]
+    }
+
+    passwordResult.textContent = password;
+}
+
+
