738cbfd6d1387c06cef612655acaf8d529717208
keshav <k21keshav05@gmail.com>
2025-05-16 23:59:14 +0530

    callback

---
+++ BroCode/A31_Callbacks/index.js
@@ -0,0 +1,22 @@
+// callback = a function that is passed as an argument
+//                    to another function.
+
+//                    used to handle asynchronous operations:
+//                    1. Reading a file
+//                    2. Network requests
+//                    3. Interacting with databases
+
+//                    "Hey, when you're done, call this next."
+
+
+hello(greeting)
+
+function hello(callback){
+    console.log("hello");
+    callback();
+}
+
+function greeting(){
+    console.log("welcome");
+}
+
