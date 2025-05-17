ce592f4f9d2da49dc65e8debb5c062f8ad623193
keshav <k21keshav05@gmail.com>
2025-05-18 01:35:52 +0530

    Function Expression

---
+++ BroCode/A36_FunctionExpression/index.js
@@ -0,0 +1,56 @@
+// function expressions = a way to define functions as
+//                        values or variables
+
+// ----------- EXAMPLE 1 -----------
+
+const hello = function(){
+    console.log("Hello");
+}
+
+hello();
+
+
+// ----------- EXAMPLE 2 -----------
+
+function hello2 (){
+    console.log("Hello2");
+}
+
+setTimeout(hello2,3000);
+
+// ----------- EXAMPLE 3 -----------
+
+setTimeout(function(){
+    console.log("Hello3");
+},3000);
+
+// ----------- EXAMPLE 4 -----------
+
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(`numbers -> `,numbers);
+const squares = numbers.map(function(element){
+    return Math.pow(element, 2);
+})
+
+const cubes = numbers.map(function(element){
+    return Math.pow(element, 3);
+});
+
+const evenNumbers = numbers.filter(function(element){
+    return element % 2 === 0;
+});
+
+const oddNumbers = numbers.filter(function(element){
+    return element % 2 !== 0;
+})
+
+const total = numbers.reduce(function(curr,next){
+    return curr + next;
+})
+
+
+console.log(`squares -> `,squares);
+console.log(`cubes -> `,cubes);
+console.log(`evenNumbers -> `,evenNumbers);
+console.log(`oddNumbers -> `,oddNumbers);
+console.log(`total -> `,total);
