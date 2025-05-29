9a30e647a42f1838dfcae29985d6840b1245f3d7
keshav <k21keshav05@gmail.com>
2025-05-29 23:40:34 +0530

    Array of Objects - Remove the last object

--- BroCode/A48_ArrayOfObjects/index.js
+++ BroCode/A48_ArrayOfObjects/index.js
@@ -3,3 +3,43 @@ const fruits = [ {name: "apple", color: "red", calories: 95},
                           {name: "banana", color: "yellow", calories: 105}, 
                           {name: "coconut", color: "white", calories: 159}, 
                           {name: "pineapple", color: "yellow", calories: 37}];
+
+console.log(`const fruits => `,fruits)
+
+// Access properties of a fruit object
+console.log(`Access properties of a fruit object`)
+console.log(`fruits[1].name -> `,fruits[1].name);
+console.log(``);
+
+
+// Add a new fruit object
+console.log(`Add a new fruit object`);
+fruits.push({name:"berries",color: "red", calories: 46});
+console.log(`fruits.push({name:"berries",color: "red", calories: 46}); => `,fruits)
+console.log(``);
+
+// Remove the last fruit object
+console.log(`Remove the last fruit object`);
+fruits.pop();
+console.log(`fruits.pop(); => `,fruits)
+console.log(``);
+
+// Remove fruit objects by indices
+console.log(`Remove fruit objects by indices`);
+console.log(``);
+
+// ---------- forEach() ----------
+console.log(`---------- forEach() ----------`);
+console.log(``);
+
+// ---------- map() ----------
+console.log(`---------- map() ----------`);
+console.log(``);
+
+// ---------- filter() ----------
+console.log(`---------- filter() ----------`);
+console.log(``);
+
+// ---------- reduce() ----------
+console.log(`---------- reduce() ----------`);
+console.log(``);
