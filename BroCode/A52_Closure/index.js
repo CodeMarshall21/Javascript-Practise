2efdc08cc7d123c44e68bc2378a5568ba2cfad53
keshav <k21keshav05@gmail.com>
2025-05-30 23:22:19 +0530

    Closure - Example 2

--- BroCode/A52_Closure/index.js
+++ BroCode/A52_Closure/index.js
@@ -26,12 +26,38 @@ console.log(``);
 
 console.log(`---------- EXAMPLE 2 ----------`);
 
+function outerCounter(){
+    let count = 0
+    console.log(`value of outerCounter count = ${count}`)
+    function innerCounter(){
+        count ++;
+        console.log(`Count increased to ${count}`);
+    }
+    function getCount(){
+        return count;
+    }
 
+    return {innerCounter, getCount}
+}
+
+console.log(outerCounter)
+
+const object = outerCounter()
+object.innerCounter();
+object.innerCounter();
+object.innerCounter();
+
+console.log(`Current Count Value => object.getCount():  `,object.getCount())
 
 console.log(``);
 
 // ---------- EXAMPLE 3 ----------
 
 console.log(`---------- EXAMPLE 3 ----------`);
+
+
+
+
+
 console.log(``);
 
