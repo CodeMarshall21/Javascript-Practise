2b408a5236a7559249358db666c0e2fffd02bcaa
keshav <k21keshav05@gmail.com>
2025-05-30 22:51:07 +0530

    Date Objects

---
+++ BroCode/A51_DateObjects/index.js
@@ -0,0 +1,10 @@
+// Date(year, month, day, hour, minute, second, ms)
+const date = new Date();
+console.log(date);
+
+console.log(`-------- Example 1 --------`)
+
+console.log(date.getFullYear(), date.getMonth(), date.getDay(), date.getHours(), date.getMinutes());
+
+
+// thats all with this rubbish....... 
