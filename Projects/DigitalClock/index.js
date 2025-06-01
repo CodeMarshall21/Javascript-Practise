4210cc3a68159624c605839df26178b05002acd4
keshav <k21keshav05@gmail.com>
2025-06-02 00:01:00 +0530

    Digital Clock - 24 hrs format to 12 hrs format

--- Projects/DigitalClock/index.js
+++ Projects/DigitalClock/index.js
@@ -1,10 +1,15 @@
 function updateClock(){
     const now = new Date();
-    const hour = now.getHours().toString().padStart(2, 0);
+    let hour = now.getHours().toString().padStart(2, 0);
+
+    const meridium = hour < 12 ? "AM" : "PM"
+    hour = hour % 12 || 12
+    hour = hour.toString().padStart(2, 0);
+
     const min = now.getMinutes().toString().padStart(2, 0);
     const sec = now.getSeconds().toString().padStart(2, 0);
     
-    const time = `${hour}:${min}:${sec}`;
+    const time = `${hour}:${min}:${sec}  ${meridium}`;
 
     document.getElementById("clock").textContent = time;
 }
