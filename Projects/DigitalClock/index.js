a4092a9e50062ad7be7ae6b7f63cf2c819b799ff
keshav <k21keshav05@gmail.com>
2025-06-01 23:55:17 +0530

    Digital Clock - JS

---
+++ Projects/DigitalClock/index.js
@@ -0,0 +1,13 @@
+function updateClock(){
+    const now = new Date();
+    const hour = now.getHours().toString().padStart(2, 0);
+    const min = now.getMinutes().toString().padStart(2, 0);
+    const sec = now.getSeconds().toString().padStart(2, 0);
+    
+    const time = `${hour}:${min}:${sec}`;
+
+    document.getElementById("clock").textContent = time;
+}
+
+updateClock();
+setInterval(updateClock,1000);
