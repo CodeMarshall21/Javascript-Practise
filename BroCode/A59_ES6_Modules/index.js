27d4fc34c5024708edf8dc1cc1ae0ed685fef6a8
keshav <k21keshav05@gmail.com>
2025-06-02 23:18:12 +0530

    ES6 Modules

--- BroCode/A59_ES6_Modules/index.js
+++ BroCode/A59_ES6_Modules/index.js
@@ -3,3 +3,14 @@
 //                          Can contain variables, classes, functions ... and more
 //                          Introduced as part of ECMAScript 2015 update
 
+import {PI, getArea, getCircumference, getVolume} from './MathUtil.js';
+
+
+console.log(`PI => `, PI);
+const cicumference = getCircumference(10);
+const area = getArea(10);
+const volume = getVolume(10);
+
+console.log(`cicumference => `,cicumference.toFixed(2));
+console.log(`area => `,area.toFixed(2));
+console.log(`volume => `,volume.toFixed(2));
