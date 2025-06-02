2860f739358f8c19fd2c73cead19e82d541e13ea
keshav <k21keshav05@gmail.com>
2025-06-02 23:38:58 +0530

    Asynchronous

---
+++ BroCode/A60_Asynchronous/index.js
@@ -0,0 +1,44 @@
+// synchronous  = Executes line by line consecutively in a sequential manner 
+//                             Code that waits for an operation to complete.
+
+// asynchronous = Allows multiple operations to be performed 
+//                               concurrently without waiting. Doesn't block the execution 
+//                               flow and allows the program to continue.
+//                               (I/O operations, network requests, fetching data)
+//                               Handled with: Callbacks, Promises, Async/Await
+
+
+console.log(`--------- Synchronous ---------`)
+
+function syncExec(){
+    setTimeout(() => console.log("Task 1"), 3000);
+    console.log("Task 2");
+    console.log("Task 3");
+    console.log("Task 4");
+}
+console.log(`function syncExec(){
+    setTimeout(() => console.log("Task 1"), 5000);
+    console.log("Task 2");
+    console.log("Task 3");
+    console.log("Task 4");
+}`)
+console.log(`calling syncExec() => `); syncExec();
+
+console.log(``);
+
+console.log(`--------- Asynchronous ---------`)
+
+function func1(callback){
+    setTimeout(() => {
+        console.log("Task 1");
+        callback();
+    }, 5000)
+}
+
+function func2(){
+    console.log("Task 2");
+    console.log("Task 3");
+    console.log("Task 4");
+}
+
+func1(func2);
