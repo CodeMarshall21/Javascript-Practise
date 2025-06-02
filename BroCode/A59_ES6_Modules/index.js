2d9695ccedda58148eb57f4152f0c72a792a4085
keshav <k21keshav05@gmail.com>
2025-06-02 23:06:41 +0530

    ES6 Modules - MathUtil.js

---
+++ BroCode/A59_ES6_Modules/index.js
@@ -0,0 +1,5 @@
+// ES6 Module = An external file that contains reusable code
+//                          that can be imported into other JavaScript files
+//                          Can contain variables, classes, functions ... and more
+//                          Introduced as part of ECMAScript 2015 update
+
