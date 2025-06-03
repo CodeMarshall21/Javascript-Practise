6908a8c151169b6c5770a8cbaa85872aa5141258
keshav <k21keshav05@gmail.com>
2025-06-03 18:20:22 +0530

    Calculator - JS

--- Projects/Calculator/index.js
+++ Projects/Calculator/index.js
@@ -3,3 +3,21 @@ const display = document.getElementById("display");
 function appendToDisplay(input){
     display.value += input;
 }
+
+function clearDisplay(){
+    display.value  = "";
+}
+
+function calculate(){
+    try{
+        let result = eval(display.value)
+        if (result === Infinity || result === -Infinity){
+            display.value = "Cannot divide by zero"
+        }
+        else{
+            display.value = result
+        }
+    }catch(error){
+        display.value = "ERROR !"
+    }
+}
