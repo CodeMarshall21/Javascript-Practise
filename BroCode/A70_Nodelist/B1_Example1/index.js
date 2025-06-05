f3c350a273da1d945922899f22b3c2952896858f
keshav <k21keshav05@gmail.com>
2025-06-06 01:44:59 +0530

    Nodelist Example 1 - JS

--- BroCode/A70_Nodelist/B1_Example1/index.js
+++ BroCode/A70_Nodelist/B1_Example1/index.js
@@ -3,3 +3,41 @@
 //                     Similar to an array, but no (map, filter, reduce) 
 //                     NodeList won't update to automatically reflect changes
 
+
+// ---------- CREATE A NODELIST ----------
+
+
+let buttons = document.querySelectorAll(".my-buttons");
+
+console.log(buttons);
+// ---------- ADD HTML/CSS PROPERTIES ----------
+
+// buttons.forEach(button => {
+//     setTimeout(() => {
+//         button.style.backgroundColor = "lightgreen";
+//         button.textContent += "😲";
+//     }
+//         , 3000)
+// })
+
+// ---------- CLICK event listener ---------- 
+
+buttons.forEach(button =>{
+    button.addEventListener("click", event =>{
+        button.style.backgroundColor = "tomato";
+    })
+});
+
+// ----------  MOUSEOVER + MOUSEOUT event listener ---------- 
+
+buttons.forEach(button =>{
+    button.addEventListener("mouseover", event =>{
+        button.style.backgroundColor = "hsl(240, 65%, 60%)";
+    })
+});
+
+buttons.forEach(button =>{
+    button.addEventListener("mouseout", event =>{
+        button.style.backgroundColor = "hsl(240, 65%, 40%)";
+    })
+});
