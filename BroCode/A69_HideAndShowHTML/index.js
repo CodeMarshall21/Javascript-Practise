f7bd59904f6e3949bd3c1dca96acf0f1d2821427
keshav <k21keshav05@gmail.com>
2025-06-06 00:46:37 +0530

    Hide And Show HTML - JS

--- BroCode/A69_HideAndShowHTML/index.js
+++ BroCode/A69_HideAndShowHTML/index.js
@@ -1,3 +1,28 @@
 const button = document.getElementById("my-button");
-const image = document.getElementById("my-image");
+const image = document.getElementById("my-img");
 
+button.addEventListener("click", event =>{
+
+    if(image.style.visibility === "hidden"){
+        image.style.visibility = "visible";
+        button.textContent = "hide";
+    }
+    else{
+        image.style.visibility = "hidden";
+        button.textContent = "Show";
+    }
+
+    /*  OR THIS CAN BE DONE
+    
+      // ----- DISPLAY -----
+    if(myImg.style.display === "none"){
+        myImg.style.display = "block";
+        myButton.textContent = "Hide";
+    }
+    else{
+        myImg.style.display = "none";
+        myButton.textContent = "Show";
+    }
+    */
+    
+})
