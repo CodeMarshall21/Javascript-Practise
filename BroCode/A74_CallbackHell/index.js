587fceb7435edc3f1865ab9cb276437eedf6b68c
keshav <k21keshav05@gmail.com>
2025-06-07 01:00:04 +0530

    Callback HELL

--- BroCode/A74_CallbackHell/index.js
+++ BroCode/A74_CallbackHell/index.js
@@ -4,4 +4,72 @@
 //                           Old pattern to handle asynchronous functions.
 //                           Use Promises + async/await to avoid Callback Hell
 
+const synchronous = document.querySelector("#synchronous p");
+const asynchronous = document.querySelector("#asynchronous p");
 
+
+function synchronousTask(){
+    function task1(){
+        setTimeout(() =>{
+            synchronous.innerHTML += "Task 1 completed<br>"
+        }, 3000)
+    };
+    function task2(){
+        setTimeout(() =>{
+            synchronous.innerHTML += "Task 2 completed<br>"
+        }, 2000)
+    };
+    function task3(){
+        setTimeout(() =>{
+            synchronous.innerHTML += "Task 3 completed<br>"
+        }, 1000)
+    };
+    function task4(){
+        setTimeout(() =>{
+            synchronous.innerHTML += "Task 4 completed<br>"
+        }, 1500)
+    };
+
+    task1();
+    task2();
+    task3();
+    task4();
+}
+
+function asynchronousTask(){
+    function task1(callback){
+        setTimeout(() =>{
+            asynchronous.innerHTML += "Task 1 completed<br>";
+            callback();
+        }, 3000)
+        
+    };
+    function task2(callback){
+        setTimeout(() =>{
+            asynchronous.innerHTML += "Task 2 completed<br>";
+            callback();
+        }, 2000)
+    };
+    function task3(callback){
+        setTimeout(() =>{
+            asynchronous.innerHTML += "Task 3 completed<br>";
+            callback();
+        }, 1000)
+    };
+    function task4(callback){
+        setTimeout(() =>{
+            asynchronous.innerHTML += "Task 4 completed<br>";
+            callback();
+        }, 1500)
+    };
+
+    task1(() =>{
+        task2(()=>{
+            task3(() =>{
+                task4(()=>{
+                    asynchronous.innerHTML += "All Tasks Completed !<br>";
+                });
+            });
+        });
+    });
+}
