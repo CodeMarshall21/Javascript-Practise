415374eb91fb0f558dd7fbec09c4a70f8dcc2d5d
keshav <k21keshav05@gmail.com>
2025-06-06 23:53:15 +0530

    Image Slider - Slider Done

--- BroCode/A73_ImageSlider/index.js
+++ BroCode/A73_ImageSlider/index.js
@@ -2,3 +2,34 @@
 
 const slider = document.querySelectorAll(".slides img");
 
+let intervalID = null;
+let slideIndex = 0;
+
+//initializeSlider()
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+    slider[slideIndex].classList.add("displaySlide");
+    intervalID = setInterval(nextSlide,1000);
+}
+
+function showSlide(slideIndex){
+    console.log(slideIndex);
+    slider.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slider[slideIndex].classList.add("displaySlide")
+}
+
+function prevSlide(){
+    clearInterval(intervalID);
+    console.log(`Slider stopped`)
+    slideIndex = (slideIndex - 1 + 3) % 3;
+    showSlide(Math.abs(slideIndex % 3));
+}
+
+function nextSlide(){
+    slideIndex = (slideIndex + 1) % 3;
+    showSlide(slideIndex % 3);
+}
