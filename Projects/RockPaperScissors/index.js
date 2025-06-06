bbcbed24771fea4a34e0453468148d4646826fff
keshav <k21keshav05@gmail.com>
2025-06-06 22:43:16 +0530

    Rock Paper Scissors - Added Score Calculation Feature

--- Projects/RockPaperScissors/index.js
+++ Projects/RockPaperScissors/index.js
@@ -4,6 +4,12 @@ const choices = ["rock","paper","scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+
+let playerScore = 0;
+let computerScore = 0;
+
 
 let result = "";
 
@@ -33,14 +39,18 @@ function playGame(playerChoice){
 
     if(result === "YOU WIN !"){
         resultDisplay.classList.add("greenDisplay");
+        playerScore ++;
     }
     else if(result === "YOU LOSE !"){
         resultDisplay.classList.add("redDisplay");
+        computerScore ++;
     }
     else{
         resultDisplay.classList.remove("greenDisplay","redDisplay");
     }
 
     resultDisplay.textContent = result;
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
 
 }
