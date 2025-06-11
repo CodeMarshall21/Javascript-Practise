15d599433c279473e3fea81b5e961a24a7ac5874
keshav <k21keshav05@gmail.com>
2025-06-11 17:39:26 +0530

    fetch - JS

--- BroCode/A79_Fetch/index.js
+++ BroCode/A79_Fetch/index.js
@@ -5,3 +5,48 @@
 //         data asynchronously over the web.
 //         fetch(url, {options})
 
+async function fetchData(){
+
+    try {
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase().trim();
+        // const pokemonFrontImage = document.getElementById("pokemonFrontImage");
+        // const pokemonBackImage = document.getElementById("pokemonBackImage");
+        const pokemonImage = document.querySelectorAll(".pokemonImage");
+
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        
+        if(!response.ok){
+        throw new Error;
+        }
+
+        const data = await response.json();
+        console.log(data)
+        const pokemonBackShinySprite = data.sprites.back_shiny;
+        const pokemonFrontShinySprite = data.sprites.front_shiny;
+
+        
+        pokemonImage[0].src = pokemonFrontShinySprite;
+        pokemonImage[1].src = pokemonBackShinySprite;
+
+        pokemonImage[0].alt = `${pokemonName} Front Image`;
+        pokemonImage[1].alt = `${pokemonName} Back Image`;
+    
+        pokemonImage[0].title = `${pokemonName} Front Image`;
+        pokemonImage[1].title = `${pokemonName} Back Image`;
+
+
+
+        pokemonImage.forEach(image => {
+            image.style.display = "block";
+        })
+        
+        
+    }catch(error){
+        console.log(error);
+    }
+
+    
+}
+
+// console.log(pokemonImage[1])
